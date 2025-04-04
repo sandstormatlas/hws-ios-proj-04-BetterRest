5eda1daad7cb20ce4f18d6bdbf238a39d26568f8
Jason Angus <jason@jasons-mba-m2>
2025-04-04 08:20:52 +0800

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Jason Angus on 4/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
