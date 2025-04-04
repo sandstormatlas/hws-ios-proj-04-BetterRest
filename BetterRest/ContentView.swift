826439aa49c9e1ae62bf01967b03141b314b725c
Jason Angus <jason@jasons-mba-m2>
2025-04-04 12:33:28 +0800

    Implement basic ui.

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Spacer()
+                Section("Wake Time") {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                        .padding()
+                    DatePicker(
+                        "Please enter a time", selection: $wakeUp,
+                        displayedComponents: .hourAndMinute
+                    )
+                    .labelsHidden()
+                    Spacer()
+                }
+            }
         }
-        .padding()
     }
 }
 
